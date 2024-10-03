2593aaf7118a8ee236a8f948e51871d71d1c8422
Jorge Garro <jorge.garroelizondo@jpschools.org>
2024-10-03 19:14:36 +0000

    saving image filtering - TODO 5

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilter(reddify);
   
 
   // do not change the below line of code
@@ -32,16 +32,39 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction) {
+  filterFunction(image);
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
 
 // TODO 5: Create the keepInBounds function
+function keepInBounds(number) {
+  return number < 0 || number > 255 ? Math.min(255,Math.max(0,number)) : number ;
+}
+console.log(keepInBounds(-30)); // should print 0
+console.log(keepInBounds(300)); // should print 255
+console.log(keepInBounds(127)); // should print 127
 
 
 // TODO 3: Create reddify function
-
+function reddify(_image) {
+  for (var i = 0; i<_image.length; i++) {
+    for (var j = 0; j<_image[i].length; j++) {
+      //2a
+      var rgbString = _image[i][j];
+      //2b
+      var rgbNumbers = rgbStringToArray(rgbString);
+      //2c
+      rgbNumbers[RED] = 200;
+      //2d
+      rgbString = rgbArrayToString(rgbNumbers);
+      //2e
+      _image[i][j] = rgbString;
+    }  
+  }
+}
 
 // TODO 6: Create more filter functions
 
